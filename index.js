f1f7832da3fdb4d5cc66f799db69db88176278f6
vinisp <aragao93.vinicius@gmail.com>
2021-10-01 17:15:41 -0300

    posts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http');
+const posts = require('./posts/posts.json')
 
 const port = process.env.PORT || 3000;
 
 http.createServer((req,res) => {
-    res.write('hello Word')
+    res.write(JSON.stringify(posts))
     res.end()
 }).listen(port, () => {
     console.log(port)
