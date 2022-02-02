e06b41bdcb03f5c4c4ecb3369cefa7b6b57ffdd2
vinisp <aragao93.vinicius@gmail.com>
2022-02-02 18:18:53 -0300

    change database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: "mysql",
-  host: "162.241.2.52",
-  username: "vinic556_crudUse",
-  password: "Drop$1091",
-  database: "vinic556_crud-commerce",
+  dialect: "postgres",
+  host: "castor.db.elephantsql.com",
+  username: "vfzmmeyk",
+  password: "OXnMa9RgDiuEFqXRgAI9mAVEczw7xqEz",
+  database: "vfzmmeyk",
   define: {
     timestamps: true,
     underscored: true,
